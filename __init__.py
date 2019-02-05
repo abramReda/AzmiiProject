458ff91ea0b29fbc7515d59818804717695b7a7c
abram reda <abramReda@github.com>
2019-02-05 22:51:32 +0000

    intialization of Azmii with main componend

--- __init__.py
+++ __init__.py
@@ -2,4 +2,15 @@
 
 
 import Hand
-import motor
+import Head
+import movement
+import RPi.GPIO as GPIO
+
+GPIO.cleanup() # reset GPIO
+
+
+LeftHand = Hand.Hand(3,5,7,8,10)
+RightHan = Hand.Hand(11,13,15,12,16)
+BigHead  = Head.Head(19,21,23,22,24)
+movesystem = movement.movement(22,24,36,35)
+
