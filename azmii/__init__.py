b426dfe8af91764aeb9242ea899a992c4acb3037
abram reda <abramReda@github.com>
2019-03-20 23:40:13 +0000

    correct spaling in RightHand variable

--- azmii/__init__.py
+++ azmii/__init__.py
@@ -10,7 +10,7 @@ GPIO.cleanup() # reset GPIO
 
 
 LeftHand = Hand.Hand(3,5,7,8,10)
-RightHan = Hand.Hand(11,13,15,12,16)
+RightHand = Hand.Hand(11,13,15,12,16)
 BigHead  = Head.Head(19,21,23,22,24)
 movesystem = movement.movement(22,24,36,35)
 
