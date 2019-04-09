fb31eb721f7b0b0cbdcb4dd4ac5c3c926dea8f32
abram reda <abramReda@github.com>
2019-04-09 09:15:45 +0000

    ther was erroer with git local :( and this is rusult

---
+++ movement.py
@@ -0,0 +1,38 @@
+
+from motor import *
+
+class movement:
+
+	def __init__(self,M11,M12,M21,M22):
+		self.rightMotor = motor(M11,M12,trigeractive = True)
+		self.leftMotor  = motor(M21,M22,trigeractive = True)
+
+
+
+	def forward(self):
+		self.rightMotor.forward()
+		self.leftMotor.forward()
+	def backward(self):
+		self.rightMotor.backward()
+                self.leftMotor.backward()
+
+
+	def right(self):
+		self.rightMotor.stop()
+                self.leftMotor.forward()
+
+	def left(self):
+		self.rightMotor.forward()
+                self.leftMotor.stop()
+
+	def stop(self):
+		self.rightMotor.stop()
+                self.leftMotor.stop()
+
+	def fastRight(self):
+                self.rightMotor.backward()
+                self.leftMotor.forward()
+        def fastLeft(self):
+                self.rightMotor.forward()
+                self.leftMotor.backward()
+
